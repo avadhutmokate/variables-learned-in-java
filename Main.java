3e27db0f3be1505bc041734735cf9f9ef59d137d
Avadhut Mokate <mokateavadhut@gmail.com>
2025-06-19 22:22:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+        public static void main (String[] args) {
+
+        //variables
+        int a = 26;
+        int b = 59;
+        int sum = a + b;
+        System.out.println(sum);
+
+        
+
+        }
+        }
